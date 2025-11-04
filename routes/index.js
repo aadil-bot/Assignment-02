07b17ef0a2e4732d3ac7c6cb177f29195d925d74
Aadil <aadil.shaikh@ontariotechu.net>
2025-11-03 22:59:58 -0500

    Second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
 });
 
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About us' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact me' });
+});
 module.exports = router;
